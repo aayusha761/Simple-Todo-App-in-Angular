bc2ce16fb42c7b080ef86cccb2d49454c6b4cf15
Aayush Arora <aroraaayush132@gmail.com>
2020-08-02 14:14:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app5';
+  temp: string;
+  todo: string[] = [];
+
+  addTodo() {
+    this.todo.push(this.temp);
+  }
+
+  displayTodo() {
+    console.log(this.todo);
+  }
 }
